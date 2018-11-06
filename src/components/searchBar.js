3ab78c912d5de73259496bac33630a4c752701b9
jacyhowe <jacyhowe@hotmail.com>
2018-11-05 18:51:02 -0700

    modified searchbar component to take onSubmit function via props.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -6,9 +6,8 @@ import { withRouter } from 'react-router-dom';
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
-        console.log('trying to handle form submit for query', query);
-        this.props.history.push('/results');
-    }
+        this.props.onSubmit(query)
+;    }
 
     renderInput(field) {
         return <input type='text' placeholder='Search Daily Smarty' {...field.input}/>
